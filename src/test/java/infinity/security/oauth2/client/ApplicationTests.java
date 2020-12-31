86a892973e70048a2254d0aa84753a95366ef0dd
ranjit <ranjit.patel@billpay.de>
2020-12-31 13:45:49 +0100

    1st commit clinet

---
+++ src/test/java/infinity/security/oauth2/client/ApplicationTests.java
@@ -0,0 +1,13 @@
+package infinity.security.oauth2.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
